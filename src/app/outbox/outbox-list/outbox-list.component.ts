9783f37e9c9e37bafa26970c709306effae7415f
denjons <dennis.j@live.se>
2017-03-23 16:09:40 +0100

    merge bootstrap

--- src/app/outbox/outbox-list/outbox-list.component.ts
+++ src/app/outbox/outbox-list/outbox-list.component.ts
@@ -11,6 +11,7 @@ import { Router } from '@angular/router';
 export class OutboxListComponent{
     
     @Input() requests: Request[];
+    
     errorMessage: string;
 
     constructor(private router: Router){
